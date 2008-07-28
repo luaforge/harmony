bc15fc56fb540fc0bb3a624e1267e896b5a8bccd
Luis Eduardo Jason Santos <jasonsantos@luaforge.net>
2008-07-28 16:06:02 +0000

    Expanded PoC Embrionary basic test

--- net.luaforge.harmony/source/lua/5.1/harmony.lua
+++ net.luaforge.harmony/source/lua/5.1/harmony.lua
@@ -1,33 +1,53 @@
 pcall(require, 'luarocks.require')
-_G=_G
-module"harmony"
+module("harmony", package.seeall)
 
-local _spheres = {}
+local _all_spheres = {}
 
-local sphere = {}
+local _sphere_class = {}
 
-function send(sphere, message)
-	if type(message)=='string' then
-		message = loadstring(message) -- compila a mensagem caso ela seja string
-	end
-	if type(message)~='function' then
-		error'message is not executable'
-	end
-	setfenv(message, sphere.environment)
-	message()	
+local _sphere_env_watch = {
+	__index = function(obj, key)
+		return rawget(rawget(obj, 1),key)
+	end;
+	__newindex = function(obj, key, value)
+		
+		print('setting', key, value)
+		
+		if type(value)=='table' then
+			value = setmetatable({value}, _sphere_env_watch)
+		end
+		rawset(rawget(obj, 1), key, value)
+	end;
+}
+
+function _sphere_class.send(sphere, message)
+        if type(message)=='string' then
+                message = loadstring(message) -- compila a mensagem
+        end
+        if type(message)~='function' then
+                error'message is not executable'
+        end
+        setfenv(message, setmetatable({ sphere.environment }, _sphere_env_watch))
+        message()
 end
 
-function subscribe(sphere, func)
-		
+function _sphere_class.subscribe(sphere, watchexpr, func)
+		if type(func)~='function' then
+                error'argument must be a function'
+        end
+        sphere.subscribers = sphere.subscribers or {}
+        sphere.subscribers[watchexpr] = sphere.subscribers[watchexpr] or {}
+        table.insert(sphere.subscribers[watchexpr], func)
 end
 
 
 function Sphere(name)
-	_spheres[name] = {
-		environment={};
-		send=sphere.send, 
-		subscribe=sphere.subscribe
-	}
-	return _spheres[name]
+        _all_spheres[name] = {
+                environment	=	{};
+                send		=	_sphere_class.send;
+                subscribe	=	_sphere_class.subscribe;
+        }
+        
+        return _all_spheres[name]
 end
 
--- net.luaforge.harmony/testsuite/basic/test.lua
+++ net.luaforge.harmony/testsuite/basic/test.lua
@@ -1,5 +1,23 @@
-package.path = [[;;./net.luaforge.harmony/source/lua/5.1/?.lua;./net.luaforge.harmony/source/lua/5.1/?/init.lua;]]
+package.path=[[;;./?.lua;./net.luaforge.harmony/source/lua/5.1/?.lua;./net.luaforge.harmony/source/lua/5.1/?/init.lua]]
 
-require'harmony'
+local h = require'harmony'
 
-print(123)
+local sphere = h.Sphere'Core'
+
+table.foreach(sphere, print)
+table.foreach(sphere.environment, print)
+
+print(os.clock())
+
+for i=1, 100 do
+sphere:send[[
+	teste = 'sim'
+	alterteste = 'nao'
+]]
+end
+
+print(os.clock())
+
+table.foreach(sphere.environment, print)
+
+print'OK'
